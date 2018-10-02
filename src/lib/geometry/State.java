9554d556f8356ceee0ecdd87b3944569033fe7cd
Jody7 <jody7777pvp@gmail.com>
2018-10-02 02:45:16 -0400

    libs and stuff

---
+++ src/lib/geometry/State.java
@@ -0,0 +1,13 @@
+package lib.geometry;
+
+import lib.util.Interpolable;
+
+public interface State<S> extends Interpolable<S> {
+    double distance(final S other);
+
+    boolean equals(final Object other);
+
+    String toString();
+
+    String toCSV();
+}
